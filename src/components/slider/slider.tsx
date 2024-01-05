b792864ef5ee6c1d910692ddf4b2d28102c39dfc
Vlad <Smirnov.ru92@mail.ru>
2024-01-05 22:37:55 +0500

    feat: swiper example

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -1,9 +1,11 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import s from './slider.module.scss'
-import { Navigation } from 'swiper/modules';
+import { A11y, Navigation, Pagination } from 'swiper';
+import {SwiperNavButtons} from "./swiper-nav-buttons/swiper-nav-buttons"
+
 
 
 const eclipseEvents = [
@@ -51,9 +53,13 @@ const eclipseEvents = [
 
 
 export const Slider = () => {
+
+
   return (
     <div style={{marginTop:'841px'}}>
+
       <Swiper slidesPerView={'auto'} freeMode navigation={true} modules={[Navigation]} style={{ width: '85%', height: '100%' }}>
+        <SwiperNavButtons />
         {eclipseEvents.map((event, index) => (
           <SwiperSlide key={index} style={{ maxWidth: '400px', height: '100%' }}>
             <div style={{margin:'0px 40px'}} className="eclipse-event">
